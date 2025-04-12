5cc5a63914e0de10ea4801fe9c6edb667c130a29
mosfi99 <mosfi99.mosfi99@gmail.com>
2025-04-12 09:09:43 +0200

    Add functions and variables till step 4

---
+++ foundations/js/assignments/rock-paper-scissors.js
@@ -0,0 +1,34 @@
+https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
+
+// This is part of The Odin Project Foundations Curriculum, where the student starts to learn JS,
+// and as arrays have not being covered yet, it will be solve without the use of them.
+
+// Goal: rock paper scissors game
+// Interface: the browser terminal
+// Players: user vs. computer
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getRandomNum() {
+	return parseFloat(Math.random().toPrecision(2));
+}
+
+function getComputerChoice() {
+	const num = getRandomNum();
+	let choice;
+
+  if (num < 0.33) {
+		choice = 'Rock';
+	} else if (num >= 0.33 && num <= 0.66) {
+	} else {
+		choice = 'Scissors';
+	}
+	return choice;
+}
+
+function getHumanChoice() {
+	const choice = prompt(`Your turn:
+  Rock, Paper or Scissors?`);
+	return choice;
+}
