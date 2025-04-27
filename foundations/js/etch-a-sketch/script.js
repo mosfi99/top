db0af911eb8a2edd89323cfa9b0688ce9d45927f
mosfi99 <mosfi99.mosfi99@gmail.com>
2025-04-27 09:47:20 +0200

    Refactor `createGrid(size)` to avoid repeating DOM queries. Return a squares array to pass it to `draw(squares)`. Create `updateRangeDisplay(size)` to separate concerns

--- foundations/js/etch-a-sketch/script.js
+++ foundations/js/etch-a-sketch/script.js
@@ -8,11 +8,17 @@ const DEFAULT_SQUARES = 25;
 const container = document.querySelector('.grid-container');
 const rangeInput = document.querySelector('#rangeSquares');
 const rangeValueDisplay = document.querySelectorAll('.range-value');
+const colorPicker = document.querySelector('#color');
 
 // GRID functions
 // n*n grid of square divs
 // need n lines, of n squares length
 function createGrid(size) {
+	updateRangeDisplay(size);
+
+	// array of square elements to iterate later on draw(squares)
+	const squares = [];
+
 	for (let i = 0; i < size; i++) {
 		for (let j = 0; j < size; j++) {
 			const square = document.createElement('div');
@@ -21,34 +27,44 @@ function createGrid(size) {
 			square.style.width = `${GRID_WIDTH / size}px`;
 			square.style.height = `${GRID_WIDTH / size}px`;
 
-			rangeValueDisplay.forEach((value) => (value.textContent = size));
+			squares.push(square);
 			container.appendChild(square);
 		}
 	}
+	return squares;
+}
+
+function updateRangeDisplay(size) {
+	// update the text on top of the range input to show the current value
+	rangeValueDisplay.forEach((value) => (value.textContent = size));
+}
+
+// draw
+function draw(squares) {
+	squares.forEach((sqr) => {
+		sqr.addEventListener('mouseover', () => {
+			sqr.style.backgroundColor = setColor();
+		});
+	});
+}
+
+// choose color
+function setColor() {
+	return colorPicker.value;
 }
 
 // EVENTS listeners
 // range input to set new grid every time input changes
 rangeInput.addEventListener('input', () => {
-	let currentSize = rangeInput.value;
+	const currentSize = rangeInput.value;
 	container.textContent = '';
-	createGrid(currentSize);
-	draw();
+	const squares = createGrid(currentSize);
+	draw(squares);
 });
 
-// draw
-function draw() {
-	const squares = document.querySelectorAll('.square');
-	squares.forEach((sqr) => {
-		sqr.addEventListener('mouseenter', () => {
-			sqr.style.backgroundColor = 'white';
-		});
-	});
-}
-
 function init() {
-	createGrid(DEFAULT_SQUARES);
-	draw();
+	const squares = createGrid(DEFAULT_SQUARES);
+	draw(squares);
 }
 
 // init
