20d424ca30daeba008831ae651e44d54458dcdc7
mosfi99 <mosfi99.mosfi99@gmail.com>
2025-04-27 11:18:09 +0200

    Implement functionality for `erase`, `clear` and `draw` buttons

--- foundations/js/etch-a-sketch/script.js
+++ foundations/js/etch-a-sketch/script.js
@@ -7,8 +7,12 @@ const DEFAULT_SQUARES = 25;
 // DOM elements
 const container = document.querySelector('.grid-container');
 const rangeInput = document.querySelector('#rangeSquares');
-const rangeValueDisplay = document.querySelectorAll('.range-value');
+const rangeValueDisplay = document.querySelectorAll('.text-range-value');
 const colorPicker = document.querySelector('#color');
+// buttons
+const drawBtn = document.querySelector('#drawBtn');
+const eraseBtn = document.querySelector('#eraseBtn');
+const clearBtn = document.querySelector('#clearBtn');
 
 // GRID functions
 // n*n grid of square divs
@@ -46,20 +50,32 @@ function updateRangeDisplay(size) {
 // Note: need to prevent default drag behavior.
 function draw(squares) {
 	let isDrawing = false; // track whether the mouse button is pressed
+	let isErasing = false; // track when erase btn clicked
 
 	// prevent drag
 	container.addEventListener('mousedown', (e) => e.preventDefault());
+
+	// draw btn clicked
+	drawBtn.addEventListener('click', () => {
+		isErasing = false;
+	});
+
+	// erase btn clicked
+	eraseBtn.addEventListener('click', () => {
+		isErasing = true;
+	});
 
 	squares.forEach((sqr) => {
 		// 1. set color only for current square
 		sqr.addEventListener('mousedown', () => {
 			isDrawing = true;
-			sqr.style.backgroundColor = setColor();
+			sqr.style.backgroundColor = isErasing ? '#FFFFFF' : setColor();
 		});
 
 		// 2. while pressing, continue setting the color for next square
 		sqr.addEventListener('mouseover', () => {
-			if (isDrawing) sqr.style.backgroundColor = setColor();
+			if (isDrawing)
+				sqr.style.backgroundColor = isErasing ? '#FFFFFF' : setColor();
 		});
 	});
 
@@ -72,13 +88,23 @@ function setColor() {
 	return colorPicker.value;
 }
 
-// EVENTS listeners
-// range input to set new grid every time input changes
-rangeInput.addEventListener('input', () => {
+// use when setting a new grid size or when clicking on clear button
+function clearContainer() {
 	const currentSize = rangeInput.value;
 	container.textContent = '';
 	const squares = createGrid(currentSize);
 	draw(squares);
+}
+
+// EVENTS listeners
+// range input to set new grid every time input changes
+rangeInput.addEventListener('input', () => {
+	clearContainer();
+});
+
+// clear container to start new draw with the same grid size
+clearBtn.addEventListener('click', () => {
+	clearContainer();
 });
 
 function init() {
